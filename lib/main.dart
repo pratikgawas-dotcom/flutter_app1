9a2a1b18ec90c168e0f97abe349c4084c58a465c
pratikgawas-dotcom <pratik.gawas@ritgoa.ac.in>
2026-09-13 19:09:29 +0530

    Change app title from 'Flutter Demo' to 'Smart Home App'

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return const MaterialApp(
+      title: 'Smart Home App',
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
